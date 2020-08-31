74c53bf6268c0f1fda9b6d4a78a6104a7ec213c2
sarthakpati <sarthak.pati@hotmail.com>
2020-08-31 16:29:23 -0400

    updated requirement for torch

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='fets',
                 'fets.models.pytorch.brainmage',
                 'fets.data',
                 'fets.data.pytorch'],
-      install_requires=['protobuf', 'pyyaml', 'grpcio', 'tqdm', 'coloredlogs', 'nibabel', 'sklearn', 'SimpleITK', 'pandas']
+      install_requires=['protobuf', 'pyyaml', 'grpcio', 'tqdm', 'coloredlogs', 'nibabel', 'sklearn', 'SimpleITK', 'pandas', 'torch>=1.6']
 )
