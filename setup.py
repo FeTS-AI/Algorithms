2c899ae38b9b7b77b7aa2b5efc93d68a9fa0357b
sarthakpati <sarthak.pati@hotmail.com>
2020-12-18 15:43:42 -0500

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fets',
-      version='0.0.1',
+      version='0.0.2',
       packages=['fets',
                 'fets.models',
                 'fets.models.pytorch',
