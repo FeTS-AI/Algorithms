5a36dabcacb65039e78a3d6faa3f71cd84cdbae8
Edwards <edwardsb@spr-gpu02.jf.intel.com>
2021-03-30 08:32:37 -0700

    removing some print statements used for debugging

--- fets/models/pytorch/brainmage/brainmage.py
+++ fets/models/pytorch/brainmage/brainmage.py
@@ -330,7 +330,7 @@ class BrainMaGeModel(PyTorchFLModel):
                     break
                 else:
                     if device.type == 'cuda':
-                        print('=== Memory (allocated; cached) : ', round(torch.cuda.memory_allocated(0)/1024**3, 1), '; ', round(torch.cuda.memory_reserved(0)/1024**3, 1))
+                        # print('=== Memory (allocated; cached) : ', round(torch.cuda.memory_allocated(0)/1024**3, 1), '; ', round(torch.cuda.memory_reserved(0)/1024**3, 1))
                     # Load the subject and its ground truth
                     # this is when we are using pt_brainmagedata
                     if ('features' in subject.keys()) and ('gt' in subject.keys()):
@@ -356,10 +356,6 @@ class BrainMaGeModel(PyTorchFLModel):
                     
                     output = self(features.float())
 
-                    # DEBUG
-                    print("\noutput info:")
-                    print("shape: ", output.shape)
-                    print("max and min sums_across channels first dim : ", torch.max(torch.sum(output, dim=1)), torch.min(torch.sum(output, dim=1)))
 
                     # Computing the loss
                     loss = self.loss_fn(output.float(), mask.float(), num_classes=self.label_channels, weights=self.dice_penalty_dict, class_list=self.data.class_list, to_scalar=False)
